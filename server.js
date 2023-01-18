df53713322ed6dad79aa7dcc1c445be2e5acdfa6
ch4r1i3 <111701536+ch4r1i3@users.noreply.github.com>
2023-01-18 18:43:14 -0500

    adding connection.js, .env, and form to server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require('path');
 const bodyParser = require('body-parser');
+// const mysql = require('mysql');
+
 const PORT = 3001;
 
 const app = express();
@@ -8,6 +10,7 @@ const app = express();
 let initialPath = path.join(__dirname, "public");
 app.use(bodyParser.json());
 app.use(express.static(initialPath));
+
 app.get('/home', (req, res) => {
     res.sendFile(path.join(initialPath, "home.html"));
 })
@@ -20,6 +23,28 @@ app.get('/register', (req, res) => {
     res.sendFile(path.join(initialPath, "register.html"));
 })
 
+app.post('/register-user', (req, res) => {
+    const { name, email, password } = req.body;
+
+    if (!name.lenght || !email.lenght || !password.lenght){
+        res.json('fill all the fields');
+    } else{
+        db("user").insert({
+            name: name,
+            email: email,
+            password: password
+        })
+        .returning(["name", "email"])
+        .then(data => {
+            res.json(data[0])
+        })
+        .catch(err => {
+            if(err.detail.includes('already existing')){
+                res.json('email already exists');
+            }
+        })
+    }
+})
 
 app.listen(PORT, () =>{
     console.log(`Server listening at ${PORT}`);
