d63066caa3ed2b8a82aac87f83ae9de4a78944a8
ch4r1i3 <111701536+ch4r1i3@users.noreply.github.com>
2023-01-18 19:29:55 -0500

    fixing code

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const path = require('path');
 const bodyParser = require('body-parser');
-// const mysql = require('mysql');
-
 const PORT = 3001;
 
 const app = express();
