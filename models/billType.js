b9ac55fcdd4489748715b2760ffa485e550cc7ec
Marissa Darr <mrsaandee@gmail.com>
2023-01-19 20:21:21 -0500

    editing database and adding seeds

---
+++ models/billType.js
@@ -0,0 +1,25 @@
+const {Model, DataTypes} = require ('sequelize');
+const { model, Sequelize } = require('../config/connection');
+
+class BillType extends Model {}
+BillType.init(
+  {
+    id:{ 
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true, 
+
+  },
+  bill_type: {
+    type:DataTypes.STRING,
+    allowNull: false
+  }
+},
+{sequelize,
+timestamps: false, 
+freezeTableName: true,
+underscored: true,
+modelName: 'Bill Type'}
+);
+module.exports = BillType;
