fd773c9a3e14c404cef73279ea6289ee761e66f7
ch4r1i3 <111701536+ch4r1i3@users.noreply.github.com>
2023-01-21 17:37:42 -0500

    refactoring code

---
+++ controllers/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+const apiRoutes = require('../controllers/index.js');
+const homeRoutes = require('./api/homeRoutes');
+
+router.use('/', homeRoutes);
+router.use('/login', apiRoutes);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require("express");
+const sequelize = require("./config/connection")
+const path = require("path");
+const exphbs = require('express-handlebars');
 
-const PORT = 3001;
+
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 
 
+const hbs = exphbs.create({});
 
-app.listen(PORT, () =>{
-    console.log(`Server listening at ${PORT}`);
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('./controllers'));
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
 });
 
 
