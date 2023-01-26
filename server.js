b66d987a8485b9325b2e0b7956bfb96d8bde1b0d
ch4r1i3 <111701536+ch4r1i3@users.noreply.github.com>
2023-01-25 22:29:15 -0500

    editing hndbs and adding home routes

--- server.js
+++ server.js
@@ -57,6 +57,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(Router);
 
 sequelize.sync({ force: true }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
+    app.listen(PORT, () => console.log('Now listening at', PORT));
 });
 
