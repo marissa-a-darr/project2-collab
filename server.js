675c0d20f4a9981ba9247149e21b2856c9129ad5
ch4r1i3 <111701536+ch4r1i3@users.noreply.github.com>
2023-01-25 21:18:26 -0500

    fixing handlebars

--- server.js
+++ server.js
@@ -3,37 +3,60 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const Router = require('./routes/api/userRoutes');
 const path = require('path');
-const sequelize = require('./config/connection');
 const { User, Payments, BillType } = require('./models');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
+const session = require('express-session');
 
 const app = express();
 
-const hbs = exphbs.create({ 
-    helpers, 
-    extname: '.hbs', 
-    layoutsDir: path.join(__dirname, 'views/layouts'), 
-    partialsDir: path.join(__dirname, 'views/partials') 
-});
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+
+const PORT = process.env.PORT || 3001;
+
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
 
 app.engine('handlebars', hbs.engine);
+// app.engine('hbs', hbs.engine({
+//     layoutsDir: __dirname + '/views/layouts',
+//     extname: 'hbs',
+//     defaultLayout: 'main',
+// }));
 app.set('view engine', 'handlebars');
+app.set('views', './public/views');
 
-app.use(cors());
-app.use(bodyParser.json());
-
-
-
-//Sync the models with the database
-sequelize.sync({ force: true }).then(() => {
-console.log('Models synced successfully');
-app.listen(3001, () => {
-console.log('Server started on port 3001');
+app.get('/login', (req, res) => {
+    res.render('login');
 });
-});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(Router);
 
-//Add the static folder
-app.use(express.static(path.join(__dirname, 'public')));
+sequelize.sync({ force: true }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
+
